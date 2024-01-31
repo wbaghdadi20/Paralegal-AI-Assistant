87499e626c0029f5fba78da3c7559fc5b527f84b
Jinduo Guo <99521795+GoldyMoon@users.noreply.github.com>
2024-01-30 22:21:25 -0800

    Front end prototype stage
    Front end prototype stage Version 0.
    Require a user friendly interface

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto;
+}
+
+nav {
+  text-align: center;
+  margin-top: 1rem;
+}
+
+</style>
