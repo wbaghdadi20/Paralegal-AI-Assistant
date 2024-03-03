ce553418722f3addf2574baee3850a3fb8ef8f6b
Jinduo Guo <99521795+GoldyMoon@users.noreply.github.com>
2024-03-03 13:21:22 -0800

    Add files via upload

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,10 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(ElementPlus)
 app.use(router)
 
 app.mount('#app')
+
