a7de69fd6495bee129a2ba993cfde5c112936bea
Aroop Biswal <aroopbiswal@gmail.com>
2024-03-03 12:25:05 -0800

    removed flask app

---
+++ Frontend/src/views/uploadDoc.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="file" ref="fileInput" @change="handleFileUpload" accept=".pdf" style="display: none" />
+    <button @click="openFileInput">Upload PDF</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    openFileInput() {
+      this.$refs.fileInput.click();
+    },
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      // Handle the uploaded file here
+      console.log(file);
+    },
+  },
+};
+</script>
